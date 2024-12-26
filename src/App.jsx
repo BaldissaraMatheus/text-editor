cb7f8caaa9cd1c2f611d12cee67c88fee2e15658
Matheus Baldissara <baldissaramatheus@gmail.com>
2024-12-26 19:38:46 -0300

    Make buttons update selection

--- src/App.jsx
+++ src/App.jsx
@@ -30,60 +30,63 @@ function App() {
   }
 
   createEffect(() => {
-    console.log(value());
+    // console.log(value());
   });
 
   // https://discourse.mozilla.org/t/how-to-get-the-caret-position-in-contenteditable-elements-with-respect-to-the-innertext/91068
-  function getEditorCaretPosition() {
+  function getEditorCaretPositions() {
     const target = mdEditor;
     target.focus(); 
     const _range = document.getSelection().getRangeAt(0); 
-    if (!_range.collapsed) { 
-      return; 
+    let length = 0;
+    if (!_range.collapsed) {
+      length = _range.endOffset - _range.startOffset;
     } 
     const range = _range.cloneRange(); 
     const temp = document.createTextNode("\0"); 
-    range.insertNode(temp); 
+    range.insertNode(temp);
     const caretposition = target.innerText.indexOf("\0"); 
     temp.parentNode.removeChild(temp); 
-    return caretposition;
+    return { start: caretposition, end: caretposition + length };
   }
 
   function getSelection() {
-    const caretPosition = getEditorCaretPosition();
+    const carentPositions = getEditorCaretPositions();
+    const caretPosition = getEditorCaretPositions().start;
     const editorText = convertToText(mdEditor.innerHTML)
     let startSelectionIndex = 0;
-    const prevLineBreak = editorText.lastIndexOf('\n', caretPosition - 1);
-    if (prevLineBreak > startSelectionIndex) {
-      startSelectionIndex = prevLineBreak;
-    }
-    const prevWhiteSpace = editorText.lastIndexOf(' ', caretPosition - 1);
-    if (prevWhiteSpace > startSelectionIndex) {
-      startSelectionIndex = prevWhiteSpace;
-    }
-    startSelectionIndex += 1;
     let endSelectionIndex = editorText.length;
-    const nextLineBreak = editorText.indexOf('\n', caretPosition);
-    if (nextLineBreak !== -1) {
-      endSelectionIndex = nextLineBreak;
-    }
-    const nextWhiteSpace = editorText.indexOf(' ', caretPosition);
-    if (nextWhiteSpace !== -1 && nextWhiteSpace < endSelectionIndex) {
-      endSelectionIndex = nextWhiteSpace;
+    if (carentPositions.end !== carentPositions.start) {
+      startSelectionIndex = carentPositions.start;
+      endSelectionIndex = carentPositions.end;
+    } else {
+      const prevLineBreak = editorText.lastIndexOf('\n', caretPosition - 1);
+      if (prevLineBreak > startSelectionIndex) {
+        startSelectionIndex = prevLineBreak;
+      }
+      const prevWhiteSpace = editorText.lastIndexOf(' ', caretPosition - 1);
+      if (prevWhiteSpace > startSelectionIndex) {
+        startSelectionIndex = prevWhiteSpace;
+      }
+      startSelectionIndex += 1;
+      const nextLineBreak = editorText.indexOf('\n', caretPosition);
+      if (nextLineBreak !== -1) {
+        endSelectionIndex = nextLineBreak;
+      }
+      const nextWhiteSpace = editorText.indexOf(' ', caretPosition);
+      if (nextWhiteSpace !== -1 && nextWhiteSpace < endSelectionIndex) {
+        endSelectionIndex = nextWhiteSpace;
+      }
     }
     const selection = editorText.substring(startSelectionIndex, endSelectionIndex)
     return { editorText, selection, startSelectionIndex, endSelectionIndex };
   }
 
-  // TODO get selected text
   // TODO update caret position
   function bold(e) {
-    // console.log(document.getSelection())
-    // return;
     const { editorText, selection, startSelectionIndex, endSelectionIndex } = getSelection();
     const newWord = `**${selection}**`;
     const newText = `${editorText.substring(0, startSelectionIndex)}${newWord}${editorText.substring(endSelectionIndex, editorText.length)}`
-    mdEditor.setSelectionRange(startSelectionIndex)
     mdEditor.innerHTML = newText;
   }
 
